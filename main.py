4b53b8a5ca12e76558638f0828d4cc7bb7f95a63
Mohammad Hayeri <matrixgram1994@gmail.com>
2020-06-07 13:56:29 +0430

    format file

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 import sys
 import requests
-
+from pprint import pprint
 
 print(sys.argv)
 URL = "https://www.virustotal.com/vtapi/v2/file/scan"
 PARAMS = {"apikey": sys.argv[1]}
 files = {"files": ("file1.exe", open(sys.argv[2], "rb"))}
 reponse = requests.post(URL, files=files, params=PARAMS)
-print(reponse.json())
+pprint(reponse.json())
